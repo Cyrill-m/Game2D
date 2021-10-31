421c9b1f8590a7c69d9b4dd8cde5c5670c40e59a
Cyrill-m <Cyrill-m@mail.ru>
2021-10-31 19:51:07 +0300

    Game loop

--- app/src/main/java/by/mkstudio/game2d/MainActivity.java
+++ app/src/main/java/by/mkstudio/game2d/MainActivity.java
@@ -1,14 +1,25 @@
 package by.mkstudio.game2d;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+/*
+ * MainActivity is the entry point of application
+ */
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // Set window size to fullscreen (hide status bar)
+        Window window = getWindow(); //get window of MainActivity
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        // Set Game object as a rendered view instead "R.layout.activity_main"
+        setContentView(new Game(this));
     }
 }
